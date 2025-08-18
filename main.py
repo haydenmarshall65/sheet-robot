1f41e66c54181d0e3c7781397914523556c98ba6
Hayden Marshall <marshall.hayden65@gmail.com>
2025-08-18 13:47:50 -0500

    fix:updates to next line properly

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from dotenv import dotenv_values
 from sheetRobot import SheetRobot
 from gspreadFactory import GspreadFactory
 
-
 if __name__ == "__main__":
     env_vars = dotenv_values(".env")
     service_account = GspreadFactory.createServiceAccount(path_to_credentials=env_vars["CREDENTIALS_JSON_FILE"])
-    spreadsheet = GspreadFactory.getWorkSheet(service_account=service_account, spreadsheet_name="Example Spreadsheet")
-    robot = SheetRobot()
-    robot.fill_in_hours(spreadsheet, "08.12.2025", "Example description", 1.5)
+    spreadsheet = GspreadFactory.getWorkSheet(service_account=service_account, spreadsheet_name="Hours_Task Tracker - Contractor - HM - 08.10 - 08.23")
+    
+    robot = SheetRobot(spreadsheet=spreadsheet)
+
+    robot.fill_in_hours(spreadsheet=spreadsheet,
+                         date="08.18.2025", 
+                         desc="More work on productivity tool", 
+                         hours=1)
--- sheetRobot.py
+++ sheetRobot.py
@@ -4,49 +4,60 @@ class SheetRobot:
     # the current spreadsheet to edit with
     _current_cell = "A9"
 
+    def __init__(self, spreadsheet):
+        if spreadsheet == None:
+            raise TypeError("Spreadsheet must be a GSpread Worksheet object")
+        self._find_next_empty_row(spreadsheet=spreadsheet)
+
     # helper function to set self._current_cell to the next empty field
     @classmethod
-    def _find_next_empty_row(self):
+    def _find_next_empty_row(self, spreadsheet):
         found_empty_row = False
         cell_number = self._current_cell
-        row = int(cell_number[1])
+        row = int(cell_number.split("A")[1])
         
         while found_empty_row == False:
             cell_number = "A" + str(row)
 
-            val = self._current_spreadsheet.acell(row).value
+            val = spreadsheet.acell(cell_number).value
 
             if val == None or val == "":
                 self._current_cell = "A" + str(row)
                 found_empty_row = True
             else:
                 row += 1
+        
+        print("(SR) [#] New current cell: " + self._current_cell)
 
     # fills in the hours on hours log spreadsheets
     @classmethod
     def fill_in_hours(self, spreadsheet, date, desc, hours, optional_row_number=None):
-        if optional_row_number:
+        if optional_row_number != None:
             row_number = optional_row_number
         else:
-            row_number = int(self._current_cell[1])
+            row_number = self._current_cell.split("A")[1]
 
         # update the date work was done
-        spreadsheet.update("A" + str(row_number), date)
+        spreadsheet.update_acell("A" + str(row_number), date)
 
         # update the desc of work done
-        spreadsheet.update("B" + str(row_number), desc)
+        spreadsheet.update_acell("B" + str(row_number), desc)
 
         # update the hours done
-        spreadsheet.update("C" + str(row_number), hours)
+        spreadsheet.update_acell("C" + str(row_number), hours)
+
+        print("(SR) [#] Updated row " + str(row_number) + "!")
+        self._find_next_empty_row(spreadsheet=spreadsheet)
     
     # removes data in the given row and sets to active row
     @classmethod
-    def delete_hours_log(self, row_number):
-        self._current_spreadsheet.update("A" + str(row_number), "")
-        self._current_spreadsheet.update("B" + str(row_number), "")
-        self._current_spreadsheet.update("C" + str(row_number), 0.0)
+    def delete_hours_log(self, spreadsheet, row_number):
+        spreadsheet.update_acell("A" + str(row_number), "")
+        spreadsheet.update_acell("B" + str(row_number), "")
+        spreadsheet.update_acell("C" + str(row_number), 0.0)
         
         self._current_cell = "A" + str(row_number)
+        print("(SR) [#] Deleted row " + str(row_number))
     
     # creates a new sheet and share it with the USER_EMAIL provided in .env
     @classmethod
