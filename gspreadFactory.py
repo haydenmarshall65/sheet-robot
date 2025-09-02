86e606551099a1d714c6bffe9aae1857010de6d4
Hayden Marshall <marshall.hayden65@gmail.com>
2025-09-02 12:29:07 -0500

    feat: added typings and cli arguments

--- gspreadFactory.py
+++ gspreadFactory.py
@@ -1,9 +1,11 @@
 import gspread
+from gspread import Client
+from gspread import Spreadsheet
 
 class GspreadFactory:
-    def createServiceAccount(path_to_credentials):
+    def createServiceAccount(path_to_credentials: str) -> Client:
         return gspread.service_account(filename=path_to_credentials)
 
-    def getWorkSheet(service_account, spreadsheet_name):
+    def getWorkSheet(service_account: Client, spreadsheet_name: str) -> Spreadsheet:
         spreadsheet = service_account.open(spreadsheet_name)
         return spreadsheet.get_worksheet(0)
